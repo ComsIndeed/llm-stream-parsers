b3e41082b847e6c47cd2b51580b4580984829948
Coms <125128463+ComsIndeed@users.noreply.github.com>
2026-05-19 09:56:08 +0800

    Added llm stream parser dart package

---
+++ packages/llm/llm_stream_parser_dart/example/llm_stream_parser_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:llm_stream_parser_dart/llm_stream_parser_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/llm/llm_stream_parser_dart/test/llm_stream_parser_dart_test.dart
@@ -0,0 +1,16 @@
+import 'package:llm_stream_parser_dart/llm_stream_parser_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
